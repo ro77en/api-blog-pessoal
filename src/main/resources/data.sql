97c46b867842a6012a3bc5fc95900b50005de7d3
ro77en <lucas.mdantas@icloud.com>
2025-05-04 20:00:00 -0300

    fix: set auto increment of category id to start at number 3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
-INSERT INTO CATEGORIES (id, title)
+INSERT INTO categories (id, title)
 VALUES (1, 'General');
 
-INSERT INTO CATEGORIES (id, title)
+INSERT INTO categories (id, title)
 VALUES (2, 'Specific');
+
+ALTER TABLE categories ALTER COLUMN id RESTART WITH 3;
