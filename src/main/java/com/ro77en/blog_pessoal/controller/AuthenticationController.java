6c933900203846abdefe37a0020d2cb4957c1d6f
ro77en <lucas.mdantas@icloud.com>
2025-05-05 20:17:11 -0300

    refact: make auth controller call jwt service directly

--- src/main/java/com/ro77en/blog_pessoal/controller/AuthenticationController.java
+++ src/main/java/com/ro77en/blog_pessoal/controller/AuthenticationController.java
@@ -1,8 +1,16 @@
 package com.ro77en.blog_pessoal.controller;
 
-import com.ro77en.blog_pessoal.security.AuthenticationService;
+import com.ro77en.blog_pessoal.dto.LoginDTO;
+import com.ro77en.blog_pessoal.dto.TokenDTO;
+import com.ro77en.blog_pessoal.security.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
-    private final AuthenticationService authenticationService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthenticationController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
+    public AuthenticationController(AuthenticationManager authenticationManager, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @PostMapping
-    public String authenticate(Authentication authentication) {
-        return authenticationService.authenticate(authentication);
+    public ResponseEntity<TokenDTO> authenticate(@RequestBody LoginDTO loginDTO) {
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(loginDTO.username(), loginDTO.password());
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(authToken);
+            String token = jwtService.generateToken(authentication);
+            return ResponseEntity.status(HttpStatus.OK).body(new TokenDTO(token));
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
