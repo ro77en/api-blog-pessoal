dea16d1dfcee0efb75b25136ce26053ba18a8e89
ro77en <lucas.mdantas@icloud.com>
2025-05-05 20:32:32 -0300

    refact: using auth0 to generate token

--- src/main/java/com/ro77en/blog_pessoal/security/JwtService.java
+++ src/main/java/com/ro77en/blog_pessoal/security/JwtService.java
@@ -1,39 +1,28 @@
 package com.ro77en.blog_pessoal.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
-    private final JwtEncoder encoder;
 
-    public JwtService(JwtEncoder encoder) {
-        this.encoder = encoder;
-    }
+    @Value("${jwt.key}")
+    private String jwtSecret;
 
     public String generateToken(Authentication authentication) {
         Instant now = Instant.now();
         long expiresAt = 3600L;
 
-        String scopes = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(" "));
-
-        var claims = JwtClaimsSet.builder()
-                .issuer("blog-pessoal")
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiresAt))
-                .subject(authentication.getName())
-                .claim("scope", scopes)
-                .build();
-
-        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        return JWT.create()
+                .withIssuer("blog-pessoal")
+                .withIssuedAt(now)
+                .withSubject(authentication.getName())
+                .withExpiresAt(now.plusSeconds(expiresAt))
+                .sign(Algorithm.HMAC256(jwtSecret));
     }
 }
